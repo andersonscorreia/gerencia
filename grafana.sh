6b2aac34d269170004356db5c9d368df9384999f
Anderson Correia <41764860+andersonscorreia@users.noreply.github.com>
2024-01-07 11:04:23 -0300

    Create grafana.sh

---
+++ grafana.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+apt install -y apt-transport-https software-properties-common wget
+
+mkdir -p /etc/apt/keyrings/
+
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
+
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee -a /etc/apt/sources.list.d/grafana.list
+
+apt update
+
+apt install grafana
+
+grafana-cli plugins install alexanderzobnin-zabbix-app
+
+systemctl start grafana-server
